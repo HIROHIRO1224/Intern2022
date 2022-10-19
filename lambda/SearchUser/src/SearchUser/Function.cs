388a2e7752aa0b87c32618f247f68fa76feebf34
HIROHIRO1224 <2001hiroto.yoshikawa1224@gmail.com>
2022-10-19 08:51:28 +0000

    update: SearchUser

--- lambda/SearchUser/src/SearchUser/Function.cs
+++ lambda/SearchUser/src/SearchUser/Function.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using Mysql.Data.MysqlClient;
 using System.Collections.Generic;
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -7,15 +8,26 @@ namespace SearchUser;
 
 public class Function
 {
+    ///RDSのエンドポイント
+    const string server="";
+    // データベース名
+    const string database="";
+    // ユーザー名
+    const string user="";
+    // パスワード
+    const string password="";
+    // 文字コード
+    const string charset="utf8";
     
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public string FunctionHandler(string input, ILambdaContext context)
+    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
     {
-        return input.ToUpper();
+        return new APIGatewayProxyResponse();
     }
 }
--- src/Intern202201AwsCdk/Intern202201AwsCdkStack.cs
+++ src/Intern202201AwsCdk/Intern202201AwsCdkStack.cs
@@ -9,22 +9,41 @@ namespace Intern202201AwsCdk
     {
         internal Intern202201AwsCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            var searchFace = new Function(this, "HelloHandler", new FunctionProps
+            Function searchFace = new Function(this, "HelloHandler", new FunctionProps
             {
                 Runtime = Runtime.DOTNET_6,
                 Code = Code.FromAsset("./lambda/HelloHandler/src/HelloHandler/bin/Debug/net6.0/publish"),
                 Handler = "HelloHandler::HelloHandler.Function::FunctionHandler",
             });
 
-            var api = new RestApi(this, "api");
-            var faceDetectMethod = api.Root.AddResource("faceSearch");
-            faceDetectMethod.AddCorsPreflight(
+            Function findUser=new Function(this,"SearchUser",new FunctionProps
+            {
+                Runtime=Runtime.DOTNET_6,
+                Code=Code.FromAsset("./lambda/SearchUser/src/SearchUser/bin/Debug/net6.0/publish"),
+                Handler="SearchUser::SearchUser.Function::FunctionHandler",
+            });
+            // APIのインスタンス生成
+            var api = new RestApi(this, "EAEMS");
+
+            // searchFaceByImageを実行するLambdaの関数の定義
+            var faceSearchMethod = api.Root.AddResource("faceSearch");
+            faceSearchMethod.AddCorsPreflight(
                 new CorsOptions{
                     AllowHeaders=new string[1]{"Content-Type"},
                     AllowMethods=new string[2]{"OPTIONS","POST"},
                     AllowOrigins=new string[1]{"*"},
             });
-            faceDetectMethod.AddMethod("POST", new LambdaIntegration(searchFace));
+
+            //ユーザーデータを取得したりする関数の定義
+            var userResource = api.Root.AddResource("user");
+            var userFindResource = userResource.AddResource("find");
+            userFindResource.AddCorsPreflight(
+                new CorsOptions{
+                    AllowHeaders=new string[1]{"Content-Type"},
+                    AllowMethods=new string[2]{"OPTIONS","POST"},
+                    AllowOrigins=new string[1]{"*"},
+            });
+            userFindResource.AddMethod("POST", new LambdaIntegration(findUser));
         }
     }
 }
